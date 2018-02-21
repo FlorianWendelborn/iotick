4ce96810145f5c72d6a836b20d92d5f680868f9f
Juliusz Skowron <juliusz.skowron@logindex.com>
2018-02-21 16:42:21 +0100

    order ticket api

--- source/api/index.js
+++ source/api/index.js
@@ -7,4 +7,7 @@ import stations from './routes/stations'
 
 const app = route.define([...orderTicket, ...stations])
 
-http.createServer(spirit.node.adapter(app)).listen(8081, () => 'listening')
+http
+	.createServer(spirit.node.adapter(app))
+	.listen(8081, () => 'listening')
+	.setTimeout(300000)
