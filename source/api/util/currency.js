8ba1168e67f28964a97543f59b1b5f804b7fb2c4
Kevin Peters <kpe@3yourmind.com>
2018-02-21 12:28:36 +0100

    api to coinmarketcap

---
+++ source/api/util/currency.js
@@ -0,0 +1,9 @@
+// var rp = require('request-promise');
+
+// const convertPriceToIota = (price) => {
+// 	// const response = await rp('https://api.coinmarketcap.com/v1/ticker/iota/?convert=EUR')
+// 	console.log(response)
+// }
+// // https://api.coinmarketcap.com/v1/ticker/iota/?convert=EUR
+// convertPriceToIota()
+// module.exports = convertPriceToIota
