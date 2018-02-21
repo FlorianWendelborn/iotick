e05c75f70be5a42fa6df32f21cfd6504bbe85702
Kevin Peters <kpe@3yourmind.com>
2018-02-21 16:10:14 +0100

    add more views

--- source/web/src/App.vue
+++ source/web/src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+		<div class="navbar">
+			<div class="container">
+				<div class="columns menu-links">
+					<router-link class="column col-4" :to="'/'">
+						Home
+					</router-link>
+					<router-link class="column col-4" :to="'/stations'">
+						Stations
+					</router-link>
+					<router-link class="column col-4" :to="'/about-us'">
+						About us
+					</router-link>
+				</div>
+			</div>
+		</div>
     <router-view/>
   </div>
 </template>
@@ -10,13 +25,31 @@ export default {
 }
 </script>
 
-<style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
+<style style="scss" scoped>
+.menu-links a {
+	height: 50px;
 	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+	color: #ffffff;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.menu-links a:active {
+	text-decoration: none;
+}
+
+.menu-links a:hover {
+	text-decoration: none;
+}
+
+.menu-links a:visited {
+	text-decoration: none;
+}
+
+.navbar {
+	background: linear-gradient(to right, #292e49, #536976);
+	height: 50px;
+	width: 100vw;
 }
 </style>
--- source/web/src/router/index.js
+++ source/web/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomeView from '../views/Home'
 import IotaGate from '@/components/IotaGate'
 import StationsView from '@/components/StationsView'
 
@@ -10,8 +10,8 @@ export default new Router({
 	routes: [
 		{
 			path: '/',
-			name: 'HelloWorld',
-			component: HelloWorld,
+			name: 'HomeView',
+			component: HomeView,
 		},
 		{
 			path: '/iotatest',
@@ -22,6 +22,10 @@ export default new Router({
 			path: '/stations',
 			name: 'Stations',
 			component: StationsView,
+		},
+		{
+			path: '*',
+			redirect: '/',
 		},
 	],
 })
