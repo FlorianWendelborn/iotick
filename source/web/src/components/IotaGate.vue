908a7a80c05fb77819ba0c9db24c1255d7367cd7
Kevin Peters <kpe@3yourmind.com>
2018-02-21 11:05:44 +0100

    put into created hook

--- source/web/src/components/IotaGate.vue
+++ source/web/src/components/IotaGate.vue
@@ -5,69 +5,71 @@
 </template>
 
 <script>
-import IOTA from "iota.lib.js";
-const seed = "jhkjgjkh787yghu3iejdutt3ijrefjdcxuyt34rtyufidi";
+import IOTA from 'iota.lib.js'
+const seed = 'jhkjgjkh787yghu3iejdutt3ijrefjdcxuyt34rtyufidi'
 
 export default {
-  name: "IOTA",
-  data() {
-    var iota = new IOTA({
-      provider: "http:///iota-node.kopciak.at:14265"
-    });
+	name: 'IOTA',
+	data() {
+		return {
+			msg: { tessts: 'xx' },
+		}
+	},
+	created() {
+		var iota = new IOTA({
+			provider: 'http:///iota-node.kopciak.at:14265',
+		})
 
-    iota.api.sendTransfer(
-      iota.utils.toTrytes(seed),
-      2,
-      18,
-      [
-        {
-          address:
-            "IJSFJTLSKI9FLKSQCJWCQIUMMAHTLMAUPF9IMRFWRKHNIDCWDIPWUGKMNLTMM9XHHV99HJKXJKVJTL9CCKKJWK9OOW",
-          value: 0,
-          message: iota.utils.toTrytes("I am buying ticket XYZ"),
-          tag: iota.utils.toTrytes("some tag")
-        }
-      ],
-      {},
-      function(error, success) {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(success);
-        }
-      }
-    );
+		iota.api.sendTransfer(
+			iota.utils.toTrytes(seed),
+			2,
+			18,
+			[
+				{
+					address:
+						'IJSFJTLSKI9FLKSQCJWCQIUMMAHTLMAUPF9IMRFWRKHNIDCWDIPWUGKMNLTMM9XHHV99HJKXJKVJTL9CCKKJWK9OOW',
+					value: 0,
+					message: iota.utils.toTrytes('I am buying ticket XYZ'),
+					tag: iota.utils.toTrytes('some tag'),
+				},
+			],
+			{},
+			function(error, success) {
+				if (error) {
+					console.error(error)
+				} else {
+					console.log(success)
+				}
+			}
+		)
 
-    // now you can start using all of the functions
-    iota.api.getNodeInfo(function(error, success) {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(success);
-      }
-    });
-    return {
-      msg: { tessts: "xx" }
-    };
-  }
-};
+		// now you can start using all of the functions
+		iota.api.getNodeInfo(function(error, success) {
+			if (error) {
+				console.error(error)
+			} else {
+				console.log(success)
+			}
+		})
+	},
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
 h2 {
-  font-weight: normal;
+	font-weight: normal;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
