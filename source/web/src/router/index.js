3cd8bad58408c249322b8952970236d3b27dd9b7
Kevin Peters <kpe@3yourmind.com>
2018-02-21 15:31:18 +0100

    add new view /stations

--- source/web/src/router/index.js
+++ source/web/src/router/index.js
@@ -2,20 +2,26 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import IotaGate from '@/components/IotaGate'
+import StationsView from '@/components/StationsView'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/iotatest',
-      name: 'IotaGate',
-      component: IotaGate
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'HelloWorld',
+			component: HelloWorld,
+		},
+		{
+			path: '/iotatest',
+			name: 'IotaGate',
+			component: IotaGate,
+		},
+		{
+			path: '/stations',
+			name: 'Stations',
+			component: StationsView,
+		},
+	],
 })
